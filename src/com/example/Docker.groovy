e30330d7c6bce0e1897af8d195883b3a60cffccc
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2023-10-04 15:05:21 +0530

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -9,12 +9,33 @@ class Docker implements Serializable {
         this.script = script
     }
 
+    // def buildDockerImage(String imageName)  {
+    //     script.echo "building the docker image..."
+    //     script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    //         script.sh "docker build -t $imageName ."
+    //         script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+    //         script.sh "docker push $imageName"
+    //     }
+    // }
+
+    //Create functions for each work
+
+    //Build image
     def buildDockerImage(String imageName)  {
         script.echo "building the docker image..."
+        script.sh "docker build -t $imageName ."
+    }
+
+    //Login
+    def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
-            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-            script.sh "docker push $imageName"
+             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
         }
     }
+
+    //push image
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
+    }
+        
 }
