a124229a056a136c6c58ea95fa8da3d3c0d1327c
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2023-10-04 14:16:59 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,12 @@
 #!/usr/bin/env groovy
 
-def call() {
+//def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t aksxy/java-maven-app:jma-2.1 .'
+       // sh 'docker build -t aksxy/java-maven-app:jma-2.1 .' 
+        sh "docker build -t $imageName ." //using $imageName as pasameter passing value from Jenkins file
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push aksxy/java-maven-app:jma-2.1'
+        sh "docker push $imageName"
      }
 }
